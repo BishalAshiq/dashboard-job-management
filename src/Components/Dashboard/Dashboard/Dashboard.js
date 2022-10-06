554986d3d1861712b56eccb56b7d6d8a7caa65f1
BishalAshiq <bishaltheboss20@gmail.com>
2022-10-06 19:12:03 +0600

    start-project

---
+++ src/Components/Dashboard/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Drawer from '../Drawer/Drawer';
+import Pages from '../Pages/Pages/Pages';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Drawer></Drawer>
+            <Pages></Pages>
+        </div>
+    );
+};
+
+export default Dashboard;
