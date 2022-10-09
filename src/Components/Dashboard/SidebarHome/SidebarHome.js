1ffe0466d23ef64dc19ab9dce2b4021957dd859c
BishalAshiq <bishaltheboss20@gmail.com>
2022-10-09 20:07:34 +0600

    added more Items

--- src/Components/Dashboard/SidebarHome/SidebarHome.js
+++ src/Components/Dashboard/SidebarHome/SidebarHome.js
@@ -6,18 +6,28 @@ import NewspaperIcon from "@mui/icons-material/Newspaper";
 import Person3OutlinedIcon from "@mui/icons-material/Person3Outlined";
 import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
 import CelebrationOutlinedIcon from "@mui/icons-material/CelebrationOutlined";
-import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import { Container } from "@mui/system";
 
 const SidebarHome = () => {
   return (
     <div>
-      <div className="sidebar-home">
+
+      <Container className="side-search-sec">
+        <div className="side_search">
+          <SearchIcon style={{color:"white"}}/>
+          <input type="text"/>
+        </div>
+      </Container>
+
+      <section className="sidebar-home">
         <HomeIcon
           color="disabled"
           style={{ width: 30, marginRight: 10, color: "rgb(247, 243, 238)" }}
         />
         <h3>Home</h3>
-      </div>
+      </section>
 
       <div>
         <section className="accordion-item">
@@ -30,15 +40,28 @@ const SidebarHome = () => {
               backgroundColor: "green",
             }}
           >
-            <AccountCircleOutlinedIcon style={{ marginRight: 4 }} />
-            <h4>User Management</h4>
-            <KeyboardArrowDownOutlinedIcon/>
+            <AccountCircleOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
+            <h4 style={{ marginRight: 4, color: "white" }}>User Management</h4>
+            <KeyboardArrowDownOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
           </div>
           <div>
-            <unl>
-              <li>Coffee</li>
-              <li>Tea</li>
-              <li>Milk</li>
+            <unl
+              style={{
+                textAlign: "justify",
+                display: "grid",
+                placeItems: "center",
+                color: "white",
+              }}
+            >
+              <li>View all users</li>
+              <li>Create user</li>
+              <li>Approval list</li>
+              <li>Role Management</li>
+              <li>Event registered user</li>
             </unl>
           </div>
         </section>
@@ -51,15 +74,28 @@ const SidebarHome = () => {
               textAlign: "center",
             }}
           >
-            <NewspaperIcon />
-            <h4>Notice & News Management</h4>
-            <KeyboardArrowDownOutlinedIcon/>
+            <NewspaperIcon style={{ marginRight: 4, color: "white" }} />
+            <h4 style={{ marginRight: 4, color: "white" }}>
+              Notice & News Management
+            </h4>
+            <KeyboardArrowDownOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
           </div>
           <div>
-            <unl>
-              <li>Coffee</li>
-              <li>Tea</li>
-              <li>Milk</li>
+            <unl
+              style={{
+                textAlign: "justify",
+                display: "grid",
+                placeItems: "center",
+                color: "white",
+              }}
+            >
+              <li>View all</li>
+              <li>Categories</li>
+              <li>Create</li>
+              <li>Approval list</li>
+              <li>Archived</li>
             </unl>
           </div>
         </section>
@@ -72,15 +108,26 @@ const SidebarHome = () => {
               textAlign: "center",
             }}
           >
-            <Person3OutlinedIcon style={{ marginRight: 10, width: 30 }} />
-            <h4>Job Management</h4>
-            <KeyboardArrowDownOutlinedIcon/>
+            <Person3OutlinedIcon style={{ marginRight: 4, color: "white" }} />
+            <h4 style={{ marginRight: 4, color: "white" }}>Job Management</h4>
+            <KeyboardArrowDownOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
           </div>
           <div>
-            <unl>
-              <li>Coffee</li>
-              <li>Tea</li>
-              <li>Milk</li>
+            <unl
+              style={{
+                textAlign: "justify",
+                display: "grid",
+                placeItems: "center",
+                color: "white",
+              }}
+            >
+              <li>View all jobs</li>
+              <li>job Configuration</li>
+              <li>Create a job</li>
+              <li>Approval list</li>
+              <li>Archived</li>
             </unl>
           </div>
         </section>
@@ -93,15 +140,26 @@ const SidebarHome = () => {
               textAlign: "center",
             }}
           >
-            <ArticleOutlinedIcon style={{ marginRight: 10, width: 30 }} />
-            <h4>Blogs & article</h4>
-            <KeyboardArrowDownOutlinedIcon/>
+            <ArticleOutlinedIcon style={{ marginRight: 4, color: "white" }} />
+            <h4 style={{ marginRight: 4, color: "white" }}>Blogs & article</h4>
+            <KeyboardArrowDownOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
           </div>
           <div>
-            <unl>
-              <li>Coffee</li>
-              <li>Tea</li>
-              <li>Milk</li>
+            <unl
+              style={{
+                textAlign: "justify",
+                display: "grid",
+                placeItems: "center",
+                color: "white",
+              }}
+            >
+              <li>View all</li>
+              <li>Categories</li>
+              <li>Create Post</li>
+              <li>Approval list</li>
+              <li>Archived</li>
             </unl>
           </div>
         </section>
@@ -114,15 +172,27 @@ const SidebarHome = () => {
               textAlign: "center",
             }}
           >
-            <CelebrationOutlinedIcon style={{ marginRight: 10, width: 30 }} />
-            <h4>Advertisement</h4>
-            <KeyboardArrowDownOutlinedIcon/>
+            <CelebrationOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
+            <h4 style={{ marginRight: 4, color: "white" }}>Advertisement</h4>
+            <KeyboardArrowDownOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
           </div>
           <div>
-            <unl>
-              <li>Coffee</li>
-              <li>Tea</li>
-              <li>Milk</li>
+            <unl
+              style={{
+                textAlign: "justify",
+                display: "grid",
+                placeItems: "center",
+                color: "white",
+              }}
+            >
+              <li>View all</li>
+              <li>Categories</li>
+              <li>Create post</li>
+              <li>Archived</li>
             </unl>
           </div>
         </section>
@@ -135,12 +205,23 @@ const SidebarHome = () => {
               textAlign: "center",
             }}
           >
-            <CelebrationOutlinedIcon style={{ marginRight: 10, width: 30 }} />
-            <h4>Miscillenious</h4>
-            <KeyboardArrowDownOutlinedIcon/>
+            <CelebrationOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
+            <h4 style={{ marginRight: 4, color: "white" }}>Miscillenious</h4>
+            <KeyboardArrowDownOutlinedIcon
+              style={{ marginRight: 4, color: "white" }}
+            />
           </div>
           <div>
-            <unl>
+            <unl
+              style={{
+                textAlign: "justify",
+                display: "grid",
+                placeItems: "center",
+                color: "white",
+              }}
+            >
               <li>Coffee</li>
               <li>Tea</li>
               <li>Milk</li>
